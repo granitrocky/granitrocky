1b4221a26612ea233ce18aa6c4602c60b682ac1f
Jonny Kenyon <J.Kenyon@ordinarygizmos.com>
2022-08-07 18:57:08 -0500

    Add cv

---
+++ cv/cv/internals.pl
@@ -0,0 +1,14 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate internals original text with physical files.
+
+
+$key = q/sec:org1a124b6/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org55d97bd/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ cv/cv/labels.pl
@@ -0,0 +1,21 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate labels original text with physical files.
+
+
+$key = q/sec:org1a124b6/;
+$external_labels{$key} = "$URL/" . q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org55d97bd/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# labels from external_latex_labels array.
+
+
+1;
+
